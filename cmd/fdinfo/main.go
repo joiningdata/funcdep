f623063e8de258c353cb93878096638d0a4fb8e5
Jeremy Jay <jeremy@pbnjay.com>
2019-02-13 14:28:48 -0500

    basic Relation parsing and some closures

---
+++ cmd/fdinfo/main.go
@@ -0,0 +1,58 @@
+// Command fdinfo reads in functional dependencies and lists various properties that can be inferred.
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+
+	"github.com/joiningdata/funcdep"
+)
+
+func main() {
+	nosep := flag.Bool("n", false, "use single-character attribute names (no separator)")
+	flag.Parse()
+
+	if *nosep {
+		funcdep.AttrSep = ""
+	}
+
+	var r io.ReadCloser = os.Stdin
+	if fn := flag.Arg(0); fn != "" {
+		f, err := os.Open(fn)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+		r = f
+	}
+
+	data, err := ioutil.ReadAll(r)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+	r.Close()
+
+	rel, err := funcdep.RelationFromString(string(data))
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+
+	fmt.Println(rel)
+
+	fmt.Println("Candidate Keys:")
+	cks := rel.CandidateKeys()
+	if len(cks) == 0 {
+		cks = rel.CandidateKeysAlt()
+	}
+	if len(cks) == 0 {
+		fmt.Println("No straightforward Candidate Keys -- Need a brute-force search!")
+	}
+	for _, ck := range cks {
+		fmt.Println("   ", ck)
+	}
+}
--- fdset.go
+++ fdset.go
@@ -1,10 +1,125 @@
 package funcdep
 
-// A Set of functional dependencies over a relation.
-type Set struct {
-	// Attrs lists all the attributes in the set of functional dependencies.
+import (
+	"fmt"
+	"strings"
+)
+
+// A Relation with a set of functional dependencies.
+type Relation struct {
+	// Name of the relation.
+	Name string
+
+	// Attrs lists all the attributes in the Relation.
 	Attrs AttrSet
 
-	// FuncDeps contains all of the functional dependencies in this set.
-	FuncDeps []FuncDep
+	// FuncDeps contains all of the functional dependencies over the Relation.
+	FuncDeps []*FuncDep
+}
+
+func (r *Relation) String() string {
+	line := r.Name + "(" + r.Attrs.String() + ")\n\n"
+	for _, fd := range r.FuncDeps {
+		line += fd.String() + "\n"
+	}
+	return strings.TrimSpace(line)
+}
+
+// RelationFromString parses a relation and optional set of functional dependencies from a string.
+func RelationFromString(desc string) (*Relation, error) {
+	lines := strings.Split(desc, "\n")
+	head := strings.TrimSpace(lines[0])
+	pidx := strings.Index(head, "(")
+	if pidx == -1 {
+		return nil, fmt.Errorf("invalid relation description")
+	}
+	r := &Relation{}
+	r.Name = head[:pidx]
+
+	// trim off parens
+	head = head[pidx+1 : len(head)-1]
+	for _, s := range strings.Split(head, AttrSep) {
+		a := Attr(strings.TrimSpace(s))
+		r.Attrs = append(r.Attrs, a)
+	}
+	for _, line := range lines[1:] {
+		line = strings.TrimSpace(line)
+		if line == "" {
+			continue
+		}
+		fd, err := FromString(line)
+		if err != nil {
+			return nil, err
+		}
+		r.FuncDeps = append(r.FuncDeps, fd)
+	}
+
+	return r, nil
+}
+
+// Closures computes the closure over every Functional Dependency.
+func (r *Relation) Closures() []*FuncDep {
+	res := make([]*FuncDep, len(r.FuncDeps))
+	for i, fd := range r.FuncDeps {
+		res[i] = r.Closure(fd)
+	}
+	return res
+}
+
+// Closure computes the closure of the given FD over the functional dependencies on this Relation.
+func (r *Relation) Closure(fd *FuncDep) *FuncDep {
+	clofd := &FuncDep{}
+	clofd.Left.AddAll(fd.Left)
+	clofd.Right.AddAll(fd.Left, fd.Right)
+
+	lastN := 0
+	n := len(clofd.Right)
+	for n != lastN {
+		for _, other := range r.FuncDeps {
+			if clofd.Right.Contains(other.Left) {
+				clofd.Right.AddAll(other.Right)
+			}
+		}
+		lastN = n
+		n = len(clofd.Right)
+	}
+	return clofd
+}
+
+// CandidateKeys returns a list of possible keys for the relation.
+// This is a somewhat efficient enumeration that relies on at least one
+// functional dependency's closure fitting the criteria.
+// If this fails to return any candidates, a brute-force search may be necessary.
+func (r *Relation) CandidateKeys() []AttrSet {
+	var ckeys []AttrSet
+	clo := r.Closures()
+	for _, cfd := range clo {
+		if len(cfd.Right) == len(r.Attrs) {
+			ckeys = append(ckeys, cfd.Left)
+		}
+	}
+	return ckeys
+}
+
+// CandidateKeysAlt returns a list of possible keys for the relation. This is
+// a somewhat efficient enumeration that gets a closure over every functional
+// dependency, then augments each with 1-2 missing attributes to fill the
+// criteria. If this fails to return any candidates, a brute-force search
+// will be necessary.
+func (r *Relation) CandidateKeysAlt() []AttrSet {
+	var ckeys []AttrSet
+	clo := r.Closures()
+	for _, cfd := range clo {
+		if len(cfd.Right) >= (len(r.Attrs) - 2) {
+			diff := r.Attrs.Difference(cfd.Right)
+			if len(diff) > 0 {
+				cfd.Left.Add(diff[0])
+			}
+			if len(diff) > 1 {
+				cfd.Left.Add(diff[1])
+			}
+			ckeys = append(ckeys, cfd.Left)
+		}
+	}
+	return ckeys
 }
